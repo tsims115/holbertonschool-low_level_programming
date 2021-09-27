0c8226e7618715b906e0c7a8e584c64e331c0dfd
tsims115 <tsims115@yahoo.com>
2021-09-27 09:23:57 -0700

    adding description for main function and added parethesis to return statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
+/**
+ * main - Prints message to terminal
+ *
+ * Return: 0 if succesful
+ */
 
 int main(void)
 {
 	char str[60];
+
 	strcpy(str, "Programming is like building a multilingual puzzle");
 	puts(str);
 
-	return 0
+	return (0);
 }
