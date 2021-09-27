3c91bb7e87914884f69893e0ace729d77f91d939
tsims115 <tsims115@yahoo.com>
2021-09-27 09:38:09 -0700

    Adding task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints message to terminal using printf command
+ *
+ * Return: 0 if runs succesfully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
