b5d97e71f06dd48cdba75d34d4e052a9cff42de4
tsims115 <tsims115@yahoo.com>
2021-09-28 11:39:26 -0700

    Adding task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints alphabet in lowercase but in reverse
+ *
+ *
+ * Return: 0 if runs succesful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
