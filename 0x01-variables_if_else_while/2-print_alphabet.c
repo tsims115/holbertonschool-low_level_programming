6efa49544f2200ec4997bac4a5c28d032f037cf2
tsims115 <tsims115@yahoo.com>
2021-09-28 08:30:41 -0700

    Adding task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ *
+ *
+ * Return: 0 if runs succesful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	printf("\n");
+	return (0);
+}
