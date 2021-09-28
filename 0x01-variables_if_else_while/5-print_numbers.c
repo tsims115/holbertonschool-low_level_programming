bd2a68116efaa3405b31cc706c24f2a83a65c883
tsims115 <tsims115@yahoo.com>
2021-09-28 09:12:27 -0700

    Adding task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints numbers 1-9
+ *
+ *
+ * Return: 0 if runs succesful
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf(i);
+	}
+	putchar('\n');
+	return (0);
+}
