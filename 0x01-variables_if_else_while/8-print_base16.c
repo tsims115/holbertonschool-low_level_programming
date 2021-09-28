c6132939e3dfbabe808f23c98e1340821d1cb08e
tsims115 <tsims115@yahoo.com>
2021-09-28 11:53:22 -0700

    Adding task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: o if succesful
+ */
+int main(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
