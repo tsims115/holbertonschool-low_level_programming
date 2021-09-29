f849023f4cbe11da6c15fec4f541abc7480b273f
tsims115 <tsims115@yahoo.com>
2021-09-29 12:17:55 -0700

    Adding task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of the number or prints 0 if 0
+ * @n: variable to check the sign
+ *
+ * Return: 1 if positive, 0 if 0, and -1 if negative
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+        }
+	_putchar('0');
+	return (0);
+}
