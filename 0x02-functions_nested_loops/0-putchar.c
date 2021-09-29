ae0788a428489acf4de6faa2e4b4b48f98a17bcd
tsims115 <tsims115@yahoo.com>
2021-09-29 08:10:09 -0700

    fixing task 0 to use _putchar instead of putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include "main.h"
 /**
  * main - prints __putchar to the screen
  *
@@ -8,6 +8,14 @@
 
 int main(void)
 {
-	printf("__putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
