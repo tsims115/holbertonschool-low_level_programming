89954db9b6f3f14b5b80f94e1b5f6a08528b4b4a
tsims115 <tsims115@yahoo.com>
2021-09-29 12:40:27 -0700

    Adding task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @num: get abs of this parameter
+ *
+ * Return: the abs of the parameter
+ *
+ */
+
+int _abs(int num)
+{
+	num = num * -1;
+	return (num);
+}
