6190eb1603b01f80608dacf8f48ecf959c2d3203
tsims115 <tsims115@yahoo.com>
2021-09-29 12:19:54 -0700

    Fixing indent to use tabs not spaces

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,10 +15,10 @@ int print_sign(int n)
 		return (1);
 	}
 	if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
-        }
+	{
+		_putchar('-');
+		return (-1);
+	}
 	_putchar('0');
 	return (0);
 }
