a7b2dfc54ff2484c70b652157e3245cf65025331
tsims115 <tsims115@yahoo.com>
2021-09-30 09:52:30 -0700

    Adding task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * add - adds two integers together
+ *@num1, @num2: numbers to add together
+ *
+ * Return: the sum of the two numbers
+ *
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
