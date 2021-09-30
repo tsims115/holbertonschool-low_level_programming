d771570b20d847a901c0552d46071ad457c9e0de
tsims115 <tsims115@yahoo.com>
2021-09-30 08:08:38 -0700

    Fixing task 9 error

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints out the 9 times table starting with 0
+ *
+ *
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int first;
+	int second;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if ((i * j) >= 10)
+                        {
+				first = i * j;
+				while (first >= 10)
+					first /= 10;
+				second = i * j;
+				second = second % 10;
+				_putchar(first + '0');
+				_putchar(second + '0');
+				_putchar(',');
+				_putchar(' ');
+				continue;
+                        }
+			_putchar(i * j + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
