26ebfe7b092292e4f4531b16714bbd03b03a2e83
tsims115 <tsims115@yahoo.com>
2021-10-03 09:24:28 -0700

    Adding task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checks to see if parameter is a digit
+ *@c: parameter to check to see if it is a digit
+ *
+ * Return: 1 if it is a digit and 0 otherwise
+ *
+ */
+
+int _isdigit(int c)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		if (c = i)
+			return (1);
+	}
+	return (0);
+}
