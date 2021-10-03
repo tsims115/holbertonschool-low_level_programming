ebfdb2c9357a2631f8f26b60b737dba8575d350d
tsims115 <tsims115@yahoo.com>
2021-10-03 15:56:41 -0700

    Adding task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - prints a line to the terminal
+ * @n: number of times the character - to be printed
+ *
+ *
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('-');
+		}
+	}
+	_putchar('\n');
+}
