487b098594a05c1a40e661ca5f569f991d6b77e4
tsims115 <tsims115@yahoo.com>
2021-10-03 09:30:01 -0700

    fixing description

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper - checks to see if parameter is a digit
+ * _isdigit - checks to see if parameter is a digit
  *@c: parameter to check to see if it is a digit
  *
  * Return: 1 if it is a digit and 0 otherwise
@@ -10,6 +10,7 @@
 int _isdigit(int c)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		if (c == (i + '0'))
