4354bddd2a130ef4999d63e539ebcdccbf23a61b
tsims115 <tsims115@yahoo.com>
2021-10-02 22:59:11 -0700

    Adding description for function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _isupper - checks to see if c is upper case
+ * @c: character to be checked
+ *
+ * Return: 1 if upper case and 0 if not uppercase
+ *
+ */
 
 int _isupper(int c)
 {
