b2107f9f2566dd8a663f2cbf59edea2d96d04ea3
tsims115 <tsims115@yahoo.com>
2021-10-05 08:41:36 -0700

    Adding task 0, README, and main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - Test C file to test how ppointers work and what they store
+ *
+ *
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# Directory for 0x05
