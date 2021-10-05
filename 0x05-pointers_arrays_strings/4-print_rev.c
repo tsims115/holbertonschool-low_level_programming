e6eb618a1c450a4f6f1988bcf2a14d4d5bf84a9a
tsims115 <tsims115@yahoo.com>
2021-10-05 11:11:48 -0700

    Adding task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - print string to console in reverse
+ * @s: string to reverse print
+ *
+ *
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	for (i = count; i >= 0; --i)
+	{
+		_putchar(*s);
+		--s;
+	}
+	_putchar('\n');
+
+}
