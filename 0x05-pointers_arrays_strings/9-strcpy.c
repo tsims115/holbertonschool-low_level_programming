740138ef85d67437f45a14c8d990fa38e7cd997b
tsims115 <tsims115@yahoo.com>
2021-10-06 08:06:30 -0700

    Adding task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcpy - copies string to another string
+ * @dest: destination of string cpy
+ * @src: String to copy
+ *
+ * Return: string to copy
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0;
+	char *string = src;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		length++;
+		dest++;
+		src++;
+	}
+	return (string);
+}
