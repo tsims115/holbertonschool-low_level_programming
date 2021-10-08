f6f316044c1eb9bfe399454915f396aaba5a94a8
tsims115 <tsims115@yahoo.com>
2021-10-08 13:56:40 -0700

    Adding task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _strncpy - copies a string
+ * @dest: copy to this variable
+ * @src: string to copy
+ * @n: copies n amount from src to dest
+ * Return: Returns a pointer to dest
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n )
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; i < n; i++)
+        {
+                if (*src == '\0')
+                {
+                        break;
+                }
+                else
+                {
+                        *dest = *src;
+                        dest++;
+                        src++;
+                }
+                length++;
+        }
+
+	for (i = 0; i < length; i++)
+		dest--;
+	return (dest);
+}
