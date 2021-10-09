1b7c6dc2abb6bd2e00c9e86f68892d128e7a24c9
tsims115 <tsims115@yahoo.com>
2021-10-08 22:02:48 -0700

    Adding task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into leet
+ * @s: string to encode to 1337
+ *
+ *
+ *
+ * Return: pointer to leet string
+ *
+ */
+
+char *leet(char *s)
+{
+	char leet[5] = {'4', '3', '0', '7', '1'};
+	char upper[5] = {'a', 'e', 'o', 't', 'l'};
+	char lower[5] = {'A', 'E', 'O', 'T', 'L'};
+	int i;
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if ((*s == upper[i]) || (*s == lower[i]))
+				*s = leet[i];
+		}
+		len++;
+		s++;
+	}
+	s -= len;
+	return (s);
+}
