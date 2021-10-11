37e29bc4c02031ba2274b28b4d9e5f546501bca4
tsims115 <tsims115@yahoo.com>
2021-10-11 10:14:13 -0700

    Returning null character instead of 0

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strchr - locates a character in a string
  * @s: string to look in
@@ -18,5 +19,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return ('\0');
+	return (NULL);
 }
