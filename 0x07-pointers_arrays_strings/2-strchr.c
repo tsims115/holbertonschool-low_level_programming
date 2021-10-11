d6f6afd868d211877d46fe80e3a8feca984e8257
tsims115 <tsims115@yahoo.com>
2021-10-11 10:28:36 -0700

    Fixing Betty

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	char *NULL = '\0';
-
 	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
 		s++;
+		if (*s == c)
+			return (s);
 	}
-	return (NULL);
+	return (0);
 }
