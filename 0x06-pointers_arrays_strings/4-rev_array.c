f23766a98670690328c37d99207b554b2989f766
tsims115 <tsims115@yahoo.com>
2021-10-10 19:51:05 -0700

    Removed comment and shortened by one more line

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,8 +10,7 @@
 
 void reverse_array(int *a, int n)
 {
-	int i;
-	int tmp;
+	int i, tmp;
 
 	for (i = 0; i < n / 2; i++)
 	{
@@ -19,22 +18,4 @@ void reverse_array(int *a, int n)
 		a[n - i - 1] = a[i];
 		a[i] = tmp;
 	}
-
-	/*
-	int i;
-	int *ptrA, *ptrB, tmp;
-
-	ptrA = a;
-	ptrB = a;
-	ptrB += n - 1;
-
-	for (i = 0; i < n / 2; i++)
-	{
-		tmp = *ptrB;
-		*ptrB = *ptrA;
-		*ptrA = tmp;
-		ptrB--;
-		ptrA++;
-	}
-	*/
 }
