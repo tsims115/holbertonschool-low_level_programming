f573f2b03d6b0197f76d2ac2a4068c8315f6fea8
tsims115 <tsims115@yahoo.com>
2021-10-11 08:21:14 -0700

    Adding task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring in string
+ * @haystack: stack of hay to search
+ * @needle: needle to find
+ *
+ * Return: first occurence of the needle otherwise null
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int j;
+
+	while (*haystack != '\0')
+	{
+		if (needle[0] == *haystack)
+		{
+			for (j = 1; needle[j] != '\0'; j++)
+			{
+				if (*(haystack + j) != needle[j])
+					break;
+			}
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (0);
+}
