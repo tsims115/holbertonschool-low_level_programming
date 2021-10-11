74ff06346e95ca6c151b307aa9fe84c03cc386c5
tsims115 <tsims115@yahoo.com>
2021-10-10 19:49:42 -0700

    Reworked task 4 and shortened the line code and made it more readable

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,6 +11,17 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[n - i - 1];
+		a[n - i - 1] = a[i];
+		a[i] = tmp;
+	}
+
+	/*
+	int i;
 	int *ptrA, *ptrB, tmp;
 
 	ptrA = a;
@@ -25,4 +36,5 @@ void reverse_array(int *a, int n)
 		ptrB--;
 		ptrA++;
 	}
+	*/
 }
