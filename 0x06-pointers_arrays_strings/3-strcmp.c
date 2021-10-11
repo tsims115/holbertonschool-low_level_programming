43bd91b6f692ae8d10a760124e9942eb6ee15c29
tsims115 <tsims115@yahoo.com>
2021-10-10 19:39:21 -0700

    Reworked task 3 by making a for loop with less total lines

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,27 +11,9 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int len1 = 0;
-	int len2 = 0;
 
-	while (*s1 != '\0')
-	{
-		len1++;
-		s1++;
-	}
-	s1 -= len1;
-	while (*s2 != '\0')
-	{
-		len2++;
-		s2++;
-	}
-	s2 -= len2;
-	for (i = 0; i < len2; i++)
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
+	for (i = 0; s1[i] != '\0'; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	return (0);
 }
