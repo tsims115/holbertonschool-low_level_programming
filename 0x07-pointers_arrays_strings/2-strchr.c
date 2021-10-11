a9f01ff5c01990050ea916509978f6512734a531
tsims115 <tsims115@yahoo.com>
2021-10-11 07:02:35 -0700

    Adding task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string to look in
+ * @c: character to locate
+ *
+ * Return: Pointer to first occurrence of character c in s or NULL if not found
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
