e2da0116b3548b8835b3acf1f6b36ccf7476b587
tsims115 <tsims115@yahoo.com>
2021-10-10 20:34:09 -0700

    Adding task 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,13 +15,9 @@ char *rot13(char *s)
 	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; alpha[j] != '\0'; j++)
-		{
-			if (s[i] == alpha[j])
-				s[i] = rot13[j];
-		}
-	}
+	for (i = 0; *(s + i) != '\0'; i++)
+		for (j = 0; j < 52; j++)
+			if (alpha[j] == *(s + i))
+				*(s + i) = rot13[j];
 	return (s);
 }
