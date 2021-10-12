1170e485bea9a8d59b7c4fc85a987506e191efbc
tsims115 <tsims115@yahoo.com>
2021-10-12 08:43:42 -0700

    In if statement return 0 instead of 1

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
  * _strlen_recursion - returns length of string
  * @s: string to create length
  *
- * Return - return length of string
+ * Return: return length of string
  *
  */
 
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 	int i = 0;
 
 	if (*s == '\0')
-		return (1);
+		return (0);
 	i = 1 + _strlen_recursion(s + 1);
 	return (i);
 }
