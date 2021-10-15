a3783607c9b8399e7053d07cefd300c85003c69d
tsims115 <tsims115@yahoo.com>
2021-10-15 12:57:47 -0700

    Adding error and return 1 if arguements are supplied

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,8 +10,17 @@
 
 int main(int argc, char **argv)
 {
-	int num1 = atoi(argv[argc - 2]);
-	int num2 = atoi(argv[argc - 1]);
+	int num1;
+	int num2;
+
+	if (argc == 1)
+        {
+                printf("Error\n");
+                return  (1);
+        }
+
+	num1 = atoi(argv[argc - 2]);
+	num2 = atoi(argv[argc - 1]);
 	printf("%d\n", num1 * num2);
 	return (EXIT_SUCCESS);
 }
