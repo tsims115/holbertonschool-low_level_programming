d4c0b8d13abfeb1c86969c7d849dc50d955be273
tsims115 <tsims115@yahoo.com>
2021-10-17 08:30:45 -0700

    Fixing betty errors to use tabs instead of spaces

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,10 +15,10 @@ int main(int argc, char **argv)
 	int num2;
 
 	if (argc - 1 <= 1)
-        {
-                printf("Error\n");
-                return  (1);
-        }
+	{
+		printf("Error\n");
+		return  (1);
+	}
 
 	num1 = atoi(argv[argc - 2]);
 	num2 = atoi(argv[argc - 1]);
