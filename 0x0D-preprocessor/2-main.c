38fb63ec168f3fcecca95123f881976a33f7c1e8
tsims115 <tsims115@yahoo.com>
2021-10-21 11:49:30 -0700

    Adding 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name of the file it was compiled from
+ *
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
