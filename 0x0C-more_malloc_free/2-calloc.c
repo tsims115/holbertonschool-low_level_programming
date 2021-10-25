944e1fb9a2d58f5cd18c3de2c6f04007b66f78a7
tsims115 <tsims115@yahoo.com>
2021-10-25 15:20:49 -0700

    Adding task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocates memory and sets everything to zero
+ * @nmemb: number of elements
+ * @size: number of bytes
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	int i;
+
+	if ((nmemb <= 0) || (size <= 0))
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL)
+	for (i = 0; i < nmemb; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
