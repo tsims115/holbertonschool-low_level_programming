3fc8296de164c4006bebade973dcd43071804291
tsims115 <tsims115@yahoo.com>
2021-10-25 15:21:22 -0700

    Adding task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
-		return (NULL)
+		return (NULL);
 	for (i = 0; i < nmemb; i++)
 	{
 		p[i] = 0;
