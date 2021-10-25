a8ef227c0a7996ba233cd5037d519d38157d6a04
tsims115 <tsims115@yahoo.com>
2021-10-25 13:25:19 -0700

    making ints unsigned

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < lens1; i++)
 		newStr[i] = s1[i];
-	for (i = 0; i < n; i++)
+	for (i = 0; i < lens2; i++)
 		newStr[lens1 + i] = s2[i];
 	newStr[lens1 + i] = '\0';
 	return (newStr);
