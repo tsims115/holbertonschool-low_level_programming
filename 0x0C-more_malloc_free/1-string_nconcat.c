59a6a398e9dc66bd58570758b896fe28f49816bf
tsims115 <tsims115@yahoo.com>
2021-10-25 14:12:15 -0700

    Making changes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (newStr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= lens1; i++)
+	for (i = 0; i < lens1; i++)
 		newStr[i] = s1[i];
 	if (n >= lens2)
 		n = lens2
