9c0cb3a7095762668fc7f7a4ebbf983f43306490
tsims115 <tsims115@yahoo.com>
2021-10-24 23:13:04 -0700

    Adding README.md, main.h and first task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates space in memory
+ * @b: how much space to use
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	void *p;
+
+	p = malloc(b);
+	if (b == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the directory for 0x0C more malloc free
