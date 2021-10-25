a318db1989c25444db43618ccf224de6c3391802
tsims115 <tsims115@yahoo.com>
2021-10-25 15:49:19 -0700

    Adding task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,6 +19,8 @@ int *array_range(int min, int max)
 		return (NULL);
 	size = max - min + 1;
 	p = malloc(size * sizeof(int));
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i <= size; i++)
 	{
 		p[i] = min + i;
