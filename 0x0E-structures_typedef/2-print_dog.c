0f4fd125feb0d5a87713643ac2988ba123e673a1
tsims115 <tsims115@yahoo.com>
2021-10-26 12:15:46 -0700

    Fixing age if statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,16 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age == NULL)
-		d->age = "(nil)";
-	if (d->owner == NULL)
-		d->owner = "(nil)"
 	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	if (d->age <= 0)
+	{
+		 printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 	printf("Owner: %s\n", d->owner);
 }
