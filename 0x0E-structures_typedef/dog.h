76d98b355d6a250a0d084ccafa5a6ec126a731e4
tsims115 <tsims115@yahoo.com>
2021-10-26 11:31:04 -0700

    Fixing betty dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog dog;
-struct dog {
+/**
+ * struct dog - makes a dog data type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner of the dog
+ *
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog;
 #endif
