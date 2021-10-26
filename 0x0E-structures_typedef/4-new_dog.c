2cc01ad5aa136dbc75429925b2cd4cdc389762cc
tsims115 <tsims115@yahoo.com>
2021-10-26 12:43:50 -0700

    Fixing betty erorors and adding if statement to check if malloc passed or failed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	dog = (struct dog*)malloc(sizeof(struct dog));
+
+	dog = (struct dog *)malloc(sizeof(struct dog));
+	if (!dog)
+		return (NULL);
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
