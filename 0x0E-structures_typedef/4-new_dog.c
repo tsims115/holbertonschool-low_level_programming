f07782f8dc1b7eaddcf0a246fea9edd47e9074ef
tsims115 <tsims115@yahoo.com>
2021-10-26 12:41:14 -0700

    Adding task 4 and updating dog.h

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	dog = (struct dog*)malloc(sizeof(struct dog));
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
