f6cc0e18a793e39521b8832c25938957b41447cd
tsims115 <tsims115@yahoo.com>
2021-10-26 22:10:27 -0700

    Trying to fix making a copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (!dog)
-		return (NULL);
+	{
+		free(dog);
+		return (dog);
+	}
 	pname = malloc(sizeof(char) * strlen(name) + 1);
 	if (!pname)
 		return (NULL);
