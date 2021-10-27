acd4d1fb6b8d298ec2ac0482b7e0b54991d8108f
tsims115 <tsims115@yahoo.com>
2021-10-26 23:46:28 -0700

    Adding task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function to use to print name
+ *
+ *
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
