2ad22fd6feebc1f53863af427ef0d481c3b1c7c9
tsims115 <tsims115@yahoo.com>
2021-10-27 10:24:26 -0700

    Adding if statement to check for NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if ((!array) || (!cmp))
-		return (NULL);
+		return (-1);
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]) != 0)
 			return (i);
