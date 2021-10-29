6118677373c4a5faa92706598d5cdae2aa43aa93
tsims115 <tsims115@yahoo.com>
2021-10-29 13:40:55 -0700

    Fixing errors in task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
 #include "variadic_functions.h"
 /**
  * print_strings - prints strings
@@ -11,17 +12,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
+	char *s;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
+		s = va_arg(args, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (s != NULL && i < n - 1)
+			printf("%s", separator);
 		if (i == n - 1)
-		{
-			printf("%s\n", va_arg(args, const char *));
-			break;
-		}
-		if (va_arg(args, const char *) == NULL)
-			printf("(nil)%s", separator);
-		printf("%s%s", va_arg(args, const char *), separator);
+			printf("\n");
 	}
+	va_end(args);
 }
