e39e053bd8d7956d3736697d56c8fbdf6247762b
tsims115 <tsims115@yahoo.com>
2021-10-29 13:52:18 -0700

    task 2 fixing if one is NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,12 +12,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
-	char *s;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(args, char *);
+		char *s = va_arg(args, char *);
 		if (s == NULL)
 		{
 			printf("(nil)");
@@ -26,8 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", s);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
-		if (i == n - 1)
-			printf("\n");
 	}
+	printf("\n");
 	va_end(args);
 }
