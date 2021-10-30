bf472b5e421db653183e4ae96205f3122dca6039
tsims115 <tsims115@yahoo.com>
2021-10-29 17:09:55 -0700

    Adding task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints everything
+ * @format: format of the string to be printed
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	va_list args;
+	int flag;
+
+       	va_start(args, format);
+	i = 0;
+	while (format && format[i])
+	{
+		flag = 0;
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int ));
+			flag = 1;
+			break;
+                case 'i':
+			printf("%d", va_arg(args, int));
+			flag = 1;
+			break;
+                case 'f':
+			printf("%f", va_arg(args, double));
+			flag = 1;
+			break;
+                case 's':
+			printf("%s", va_arg(args, char *));
+			flag = 1;
+			break;
+		}
+		if (flag == 1 && format[i + 1])
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
