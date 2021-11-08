4650fed7efa2917b6fcc58bf78fe4ce454b8b8f2
tsims115 <tsims115@yahoo.com>
2021-11-08 15:21:41 -0800

    Reworking task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * add_node_end - add a node to the end of the linked list
  * @head: head of the linked list
@@ -12,23 +13,19 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *node;
 	list_t *last;
-	char *s;
-	int i;
+	unsigned int i = strlen(str);
 
+	if (!head || !str)
+		return (NULL);
 	last = *head;
 	node = (list_t *)malloc(sizeof(list_t));
 
 	if (node == NULL)
+	{
+		free(node);
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	s = malloc(sizeof(char) * i);
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		s[i] = str[i];
-	node->str = s;
+	}
+	node->str = strdup(str);
 	node->len = i;
 	node->next = NULL;
 
