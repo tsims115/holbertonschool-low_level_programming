5c03449047d8356eca3403b1ae60fa7ec05f4d9d
tsims115 <tsims115@yahoo.com>
2021-11-08 15:12:30 -0800

    Adding check to see if head or str is null return null

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,6 +15,8 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *node;
 	unsigned int i;
 
+	if (!head || !str)
+                return (NULL);
 	node = (list_t *)malloc(sizeof(list_t));
 	if (!node)
 	{
