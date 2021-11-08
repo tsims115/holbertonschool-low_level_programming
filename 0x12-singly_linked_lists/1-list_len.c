ff4bf21a51dbc35767c842c7fb5d9b5aecf535a0
tsims115 <tsims115@yahoo.com>
2021-11-07 23:39:02 -0800

    Adding task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * list_len - returns number of elements in a linked list
+ * @h: head of linked list
+ * Return: number of elements in a list
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *p;
+	size_t n;
+
+	n = 0;
+	for (p = h; p != NULL; p = p->next)
+		n++;
+	return (n);
+}
