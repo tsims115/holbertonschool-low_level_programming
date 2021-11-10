6928874d63877f106a951183c5af151f8d6ef12a
tsims115 <tsims115@yahoo.com>
2021-11-10 15:49:00 -0800

    Adding task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - free list_t list
+ * @head: head of list to free
+ */
+
+void free_listint2(listint_t **head)
+{
+        listint_t *cur_node;
+
+        while (*head != NULL)
+        {
+		cur_node = (*head)->next;
+                free(*head);
+                *head = cur_node;
+        }
+	free(cur_node);
+	*head = NULL;
+}
