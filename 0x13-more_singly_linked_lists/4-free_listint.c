84fa0ce3b26ede872d1f96acd993e610c4cb4161
tsims115 <tsims115@yahoo.com>
2021-11-10 11:17:16 -0800

    Adding task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint - free list_t list
+ * @head: head of list to free
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *cur_node;
+
+	while (!head)
+	{
+		cur_node = head->next;
+		free(head);
+		head = cur_node;
+	}
+}
