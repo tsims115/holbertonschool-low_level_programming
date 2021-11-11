1793107b6c694112cbf44ace41aee2b6529d9aad
tsims115 <tsims115@yahoo.com>
2021-11-11 09:47:52 -0800

    chsaning if statement to use idx == 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	if (idx == 0 && *head == NULL)
+	if (idx == 0)
 	{
 		new_node->next = (*head)->next;
 		*head = new_node;
