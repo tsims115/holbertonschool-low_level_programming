6145b5bd51866cd5fe12ccd96f56fed91d610198
tsims115 <tsims115@yahoo.com>
2021-11-11 13:56:04 -0800

    fixing n assignment

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,10 +10,11 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *new_head;
-	int n = (*head)->n;
+	int n;
 
 	if (*head == NULL)
 		return (0);
+	n = (*head)->n;
 	new_head = (*head)->next;
 	free(*head);
 	*head = new_head;
