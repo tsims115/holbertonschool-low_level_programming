592285d7a13e4ed7c5cb0d3483d83cc5da9a449a
tsims115 <tsims115@yahoo.com>
2021-11-11 09:39:04 -0800

    chaning if consition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,15 +16,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(listint_t));
-	if (head == NULL)
-		return (NULL);
 	if (!new_node)
 	{
 		free(new_node);
 		return (NULL);
 	}
 	new_node->n = n;
-	if (idx == 0)
+	if (idx == 0 && head == NULL)
 	{
 		new_node->next = (*head)->next;
 		*head = new_node;
