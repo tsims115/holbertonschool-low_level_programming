347f40b6ee6ef96b395e01768d3d67eca51d05e2
tsims115 <tsims115@yahoo.com>
2021-11-11 09:37:56 -0800

    Adding task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(listint_t));
+	if (head == NULL)
+		return (NULL);
 	if (!new_node)
 	{
 		free(new_node);
