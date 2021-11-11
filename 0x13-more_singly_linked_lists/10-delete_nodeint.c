c7051e6346b1258ab9a9b583e8a0fa3eaa7e3882
tsims115 <tsims115@yahoo.com>
2021-11-10 23:26:15 -0800

    changing first return to -1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	if (!*head)
-		return (1);
+		return (-1);
 	if (i == index)
 	{
 		next_node = cur_node->next;
