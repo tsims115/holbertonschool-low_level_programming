89f719312712a3d33b0862d518942158d1737133
tsims115 <tsims115@yahoo.com>
2021-11-11 14:14:09 -0800

    adding if statement to check if cur node is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,6 +31,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	while (i <= idx)
 	{
+		if (cur_node == NULL)
+			return (NULL);
 		if (i == idx - 1)
 		{
 			new_node->next = cur_node->next;
