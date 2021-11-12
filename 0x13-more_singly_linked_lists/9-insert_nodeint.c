b13317ad98f31345db77ed20e4a10181507e4acd
tsims115 <tsims115@yahoo.com>
2021-11-11 20:59:39 -0800

    if head is NULL return NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,14 +14,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	if (head == NULL)
-		return (NULL);
 	new_node->n = n;
 	if (idx == 0)
 	{
