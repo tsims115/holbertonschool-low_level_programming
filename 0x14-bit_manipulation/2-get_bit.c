5d056c9ea8e535e13f788567257620e0f7d42fde
tsims115 <tsims115@yahoo.com>
2021-11-12 19:55:41 -0800

    Adding task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * get_bit - get bit of n at  index
+ * @n: number to get bit of
+ * @index: index of bit to get
+ * Return: value of the bit at the index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = (n >> index) & 1;
+
+	return  (bit);
+}
