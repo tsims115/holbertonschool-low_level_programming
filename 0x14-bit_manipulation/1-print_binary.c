a6dc6422c173aa8f1cc7cff3373d13b66bbc5863
tsims115 <tsims115@yahoo.com>
2021-11-12 19:47:50 -0800

    Adding task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,40 +1,15 @@
 #include <stdio.h>
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * print_binary - prints the number in binary
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	int i;
-	int flag = 0;
-	unsigned int res;
-	int j;
-
-	if (n == 0)
-		_putchar('0');
-	for (i = 15; i >= 0; i--)
+	if (n > 1)
 	{
-		res = 1;
-		for (j = 1; j <= i; j++)
-		{
-			if (i == 0)
-			{
-				res = 1;
-				break;
-			}
-			res = res * 2;
-		}
-		if (n >= res)
-		{
-			flag = 1;
-			_putchar('1');
-			n = n - res;
-		}
-		else if (flag == 1)
-		{
-			_putchar('0');
-		}
+		print_binary(n >> 1);
 	}
+	_putchar((n & 1) + '0');
 }
