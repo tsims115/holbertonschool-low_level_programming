cd4dce84253a1f5bf3afd4c04b946193f14457fb
tsims115 <tsims115@yahoo.com>
2021-11-17 10:24:30 -0800

    Adding check for n

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -34,6 +34,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (n == -1)
 		return (0);
 	n = printf("%s", buf);
+	if (n == -1)
+		return (0);
 	free(buf);
 	close(fd);
 	return (n);
