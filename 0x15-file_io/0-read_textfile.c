b8530036de7204fb8243763d5477be2a5727d047
tsims115 <tsims115@yahoo.com>
2021-11-17 10:23:34 -0800

    Using printf

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -33,7 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	n = read(fd, buf, letters);
 	if (n == -1)
 		return (0);
-	n = write(1, buf, letters);
+	n = printf("%s", buf);
 	free(buf);
 	close(fd);
 	return (n);
