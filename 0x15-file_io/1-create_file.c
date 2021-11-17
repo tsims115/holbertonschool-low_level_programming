abff0ede7faa85651bd0e6d92a2dcf6a1cb5ab1b
tsims115 <tsims115@yahoo.com>
2021-11-17 14:22:48 -0800

    Taking out check for write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,16 +20,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
 		return (1);
 
-	n = write(fd, text_content, n);
-	if (n == -1)
-		return (-1);
+	write(fd, text_content, n);
 	close(fd);
 	return (1);
 
