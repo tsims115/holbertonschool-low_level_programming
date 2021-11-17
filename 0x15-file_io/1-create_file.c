9eb2607b191e1df79d939e441678823697f3ad53
tsims115 <tsims115@yahoo.com>
2021-11-17 14:33:05 -0800

    Making text_content empty string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,11 +25,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (1);
+		text_content = "";
 
-	if (write(fd, text_content, n) == -1)
+	if (write(fd, text_content, strlen(text_content)) == -1)
 		return (-1);
-	close(fd);
 	return (1);
 
 }
