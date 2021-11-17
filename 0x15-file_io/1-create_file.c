f391e8a22b20a7dbb7cb5739d1a59d3293077422
tsims115 <tsims115@yahoo.com>
2021-11-17 14:29:44 -0800

    Adding a write check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-	write(fd, text_content, n);
+	if (write(fd, text_content, n) == -1)
+		return (-1);
 	close(fd);
 	return (1);
 
