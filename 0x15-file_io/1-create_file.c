116496863dc43dbd84d4a713331bae0e55e87fcb
tsims115 <tsims115@yahoo.com>
2021-11-17 11:04:49 -0800

    Making it return -1 instead of 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		return (0);
+		return (-1);
 	n = write(fd, text_content, sizeof(text_content));
 	if (n == -1)
 		return (-1);
