a02d1119843108eb2db28cbc7fbc768ac9e420e6
tsims115 <tsims115@yahoo.com>
2021-11-18 12:51:49 -0800

    Changing while loop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,8 +30,9 @@ int main(int argc, char **argv)
 	if (dest == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
 		exit(99);
-	while ((nchars = read(srcd, buf, 1024)) != 0)
+	while (nchars != 0)
 	{
+		nchars = read(srcd, buf, 1024);
 		if (nchars == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
