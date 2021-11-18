f06f482b6eaf5c363b00d868bebec6c3b0b72ce0
tsims115 <tsims115@yahoo.com>
2021-11-17 16:18:02 -0800

    Changing while loop conditon

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 	if (dest == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]),
 		exit(98);
-	while (nchars == 1024)
+	while (nchars != 0)
 	{
 		nchars = read(srcd, buf, 1024);
 		if (nchars == -1)
