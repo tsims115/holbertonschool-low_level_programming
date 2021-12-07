ce4b538f37ec31cbb72a40896f6271d7b0e355b5
tsims115 <tsims115@yahoo.com>
2021-12-06 23:51:36 -0800

    Adding task 4

---
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *cur_node;
+
+	while (cur_node != NULL)
+	{
+		cur_node = head->next;
+		free(head);
+		head = cur_node;
+	}
+}
