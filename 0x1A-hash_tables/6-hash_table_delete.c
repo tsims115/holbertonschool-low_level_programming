9e5f229eb5881f1d02f645562a2a91fc9c0375aa
tsims115 <tsims115@yahoo.com>
2021-12-11 23:36:55 -0800

    Fixing betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -15,7 +15,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
-	        while (node != NULL)
+		while (node != NULL)
 		{
 			tmp = node;
 			node = tmp->next;
