49187d8d784219aa3a0d0d7fce15b42bd7cc2247
tsims115 <tsims115@yahoo.com>
2021-12-11 19:13:38 -0800

    Freeing node if code fails

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,16 +6,21 @@
  * @value: value associated with the key
  * Return: 1 if success or -1 if fails
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node = malloc(sizeof(hash_node_t));
 	hash_node_t *tmp = ht->array[key_index((const unsigned char *)key, ht->size)];
 
 	if (node == NULL)
+	{
+		free(node);
 		return (0);
+	}
 	if (strcmp("", key) == 0)
+	{
+		free(node);
 		return (0);
+	}
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = NULL;
