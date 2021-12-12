564ff0912dfb8a9b5ae31ecbd329cdeb63b42c69
tsims115 <tsims115@yahoo.com>
2021-12-11 23:52:39 -0800

    Fixing betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -16,7 +16,7 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
-	        while (node != NULL)
+		while (node != NULL)
 		{
 			if (flag == 1)
 				printf(", ");
