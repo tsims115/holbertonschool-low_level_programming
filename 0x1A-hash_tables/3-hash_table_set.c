87d56a8f69e49017ec86aa7d22901f631474e468
tsims115 <tsims115@yahoo.com>
2021-12-11 19:34:53 -0800

    Adding free statements

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,6 +25,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(key, tmp->key) == 0)
 		{
+			free(node->key);
+			free(node);
 			free(tmp->value);
 			tmp->value = strdup(value);
 		}
