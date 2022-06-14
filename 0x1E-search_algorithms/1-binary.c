1f9ee07c3069c6cad2490bade42b9984e87cbe8e
Trenton Sims <tsims115@yahoo.com>
2022-06-14 16:21:07 -0500

    Adding upadated .h

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
         if (ls > rs)
             return (-1);
         mp = (ls + rs) / 2;
-        printf("Searching in array: ", mp, array[mp]);
+        printf("Searching in array: ");
 		for (j = ls; j <= rs; j++)
         {
             printf("%d", array[j]);
