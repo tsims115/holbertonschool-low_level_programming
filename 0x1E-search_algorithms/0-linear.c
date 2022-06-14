d7ab201d4fabbcb057c4e0ac34fc7dbf56ac2cf1
Trenton Sims <tsims115@yahoo.com>
2022-06-14 15:13:03 -0500

    Adding task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * linear_search - seraches for a value linearly 
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index of the value found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < (int)size; i++) 
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return i;
+			break;
+		}
+	}
+	return -1;
+}
+
